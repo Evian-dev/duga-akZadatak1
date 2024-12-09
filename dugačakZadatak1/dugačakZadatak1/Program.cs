ae89f1df331e0edac8b59def1202ff0713a94369
Evian-dev <evian.rebic@gmail.com>
2024-12-09 13:37:22 +0100

    Add files via upload

---
+++ dugačakZadatak1/dugačakZadatak1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dugačakZadatak1
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+        }
+
+        
+            public class Neboder
+        {
+            private double visina;
+            private int brojKatova;
+
+            public double Visina
+            {
+                get => visina;
+                set => visina = value;
+            }
+
+            public int BrojKatova
+            {
+                get => brojKatova;
+                set => brojKatova = value;
+            }
+
+            public override string ToString() => $"Visina: {visina} m, Broj katova: {brojKatova}, Prosječna visina kata: {visina / brojKatova} m";
+
+            public Neboder(double visina, int brojKatova)
+            {
+                this.visina = visina;
+                this.brojKatova = brojKatova;
+            }
+        }
+
+        }
+}
+
